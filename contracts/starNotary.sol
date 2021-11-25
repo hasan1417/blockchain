c397f32b9ac8742ff82c27d26d847b1d355a7c82
hassan <your_name@domain.com>
2021-11-26 00:30:24 +0300

    pre final

--- contracts/starNotary.sol
+++ contracts/starNotary.sol
@@ -1,5 +1,22 @@
 pragma solidity ^0.8.10;
 
-contract StarNotary { 
+import './ERC721Token.sol';
+
+contract StarNotary is ERC721Token { 
+
+    struct Star{
+        string name;
+    }
+
+    mapping(uint256 => Star) public tokenIdToStarInfo;
+
+    function createStar(string memory _name, uint256 _tokenId) public{
+        Star memory newStar = Star(_name);
+
+        tokenIdToStarInfo[_tokenId] = newStar;
+
+        ERC721Token.mint(_tokenId);
+        
+    }
 
 }
